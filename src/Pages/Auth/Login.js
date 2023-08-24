5fb904596ce2629a5a2823c392c565a3b75ea7b7
Paras Arya <parasarya289@gmail.com>
2023-08-24 18:37:09 +0530

    style: Login.css

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,20 +1,30 @@
+import { useRef } from "react";
+import { loginUser } from "../../Helpers/authHelpers";
 import "./Login.css";
+
 export const Login = () => {
-  const loginHandler = (e) => {
-    e.preventDefault();
+  const formRef = useRef(null);
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(formRef.current);
+    const userCred = Object.fromEntries(formData.entries());
+    console.log(userCred);
+    loginUser(userCred);
   };
 
   return (
     <div className="login">
-      <form className="login-form" onSubmit={loginHandler}>
-        <label htmlFor="email">
-          Email
-          <input type="text" name="email" placeholder="Email" />
+      <form ref={formRef} className="login-form" onSubmit={loginHandler}>
+        <h1 style={{margin:0}}>Signin</h1>
+        <label htmlFor="username">
+          Username
+          <input type="text" name="username" placeholder="Username" required/>
         </label>
 
-        <label htmlFor="password">
-          Password
-          <input type="text" name="password" placeholder="Password" />
+        <label htmlFor="email">
+          Email
+          <input type="email" name="email" placeholder="Email" required/>
         </label>
 
         <button type="submit">Submit</button>
