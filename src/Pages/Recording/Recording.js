13e959536c7bdff43e3797506d6a658eee0236df
Paras Arya <parasarya289@gmail.com>
2023-08-27 00:18:42 +0530

    feat: single recording page

--- src/Pages/Recording/Recording.js
+++ src/Pages/Recording/Recording.js
@@ -1,5 +1,35 @@
-export const Recording = () =>{
-    return(
-        <div>Recording</div>
-    )
-}
+import { useParams } from "react-router-dom";
+import { useRecordings } from "../../Context/recordingContext";
+import { BsDownload } from "react-icons/bs";
+import "./Recording.css";
+
+export const Recording = () => {
+  const { recordingId } = useParams();
+
+  const {
+    recordings: { recordings },
+  } = useRecordings();
+
+  const foundRecording = recordings.find(({ id }) => id == recordingId);
+
+  return (
+    <div className="recording">
+      <video controls>
+        <source
+          src={URL.createObjectURL(foundRecording.vid)}
+          type="video/webm"
+        />
+      </video>
+      <div>
+        <p>{foundRecording.title}</p>
+        <a
+          href={URL.createObjectURL(foundRecording.vid)}
+          download={`${foundRecording.title}.webm`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <BsDownload />
+        </a>
+      </div>
+    </div>
+  );
+};
