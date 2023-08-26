34044882ad2d390bc4b944e8f80811d3ade1c862
Paras Arya <parasarya289@gmail.com>
2023-08-26 23:59:02 +0530

    feat: moved navbar to app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 import { ProtectedRoute } from "./Components/ProtectedRoute/ProtectedRoute";
 import { Home } from "./Pages/Home/Home";
 import { Login } from "./Pages/Auth/Login";
+import { Recording } from "./Pages/Recording/Recording";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
@@ -14,6 +17,14 @@ function App() {
           element={
             <ProtectedRoute>
               <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/recording/:recordingId"
+          element={
+            <ProtectedRoute>
+              <Recording />
             </ProtectedRoute>
           }
         />
